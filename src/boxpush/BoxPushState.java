fcde1dabad4effa0dc9a60d20b90589ff3f319d4
Unknown <alex-w93@gmx.de>
2018-11-26 09:42:21 +0100

    Sensors are now updated when needed

--- src/boxpush/BoxPushState.java
+++ src/boxpush/BoxPushState.java
@@ -1,6 +1,12 @@
 package boxpush;
 
+import execution.Executor;
+import execution.Mode;
 import execution.State;
+import lejos.hardware.Button;
+import lejos.hardware.Key;
+import lejos.hardware.KeyListener;
+import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import robot.SensorController;
 
@@ -40,6 +46,22 @@ public class BoxPushState extends State {
 	
 	@Override
 	public void mainloop() {
+		// make program stoppable
+		Button.ESCAPE.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyPressed(Key k) {
+				Sound.beep();
+				Executor.get().requestChangeMode(Mode.ModeMenu);
+			}
+
+			@Override
+			public void keyReleased(Key k) {
+				//Executor.get().requestChangeMode(Mode.ModeMenu);
+			}
+			
+		});
+		
 		// TODO: implement the following comments
 		
 		// find box: -drive slowly
@@ -64,9 +86,12 @@ public class BoxPushState extends State {
 		//			- turn 90° right
 		pmotors.turnRight(90);
 		//			- drive till the box is at the wall (or both touch sensors are activated)
+		sensorController.tick();
 		while (!sensorController.isLeftTouching() && !sensorController.isRightTouching()) {
 			pmotors.goForward();
+			sensorController.tick();
 		}
+		pmotors.quickStop();
 		//			- pull back a little (e.g. 3cm)
 		pmotors.travel(-3);
 		//			- turn 90° left
@@ -76,16 +101,20 @@ public class BoxPushState extends State {
 		//			- turn 90° right
 		pmotors.turnRight(90);
 		//			- drive until both touch sensors hit the wall
+		sensorController.tick();
 		while (!sensorController.isLeftTouching() && !sensorController.isRightTouching()) {
 			pmotors.goForward();
+			sensorController.tick();
 		}
+		pmotors.quickStop();
 		//			- pull back a bit
-		pmotors.travel(-3);
+		pmotors.travel(-2);
 		//			- turn 90° right
 		pmotors.turnRight(90);
 		//			- drive forward until box is at the wall
 		while (!sensorController.isLeftTouching() && !sensorController.isRightTouching()) {
 			pmotors.goForward();
 		}
+		pmotors.quickStop();
 	}
 }
